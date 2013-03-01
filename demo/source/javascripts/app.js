a9b750864e6d11a45110a2f1a18f30b2aeb8f607
Steven J. Dale <lifeinchords@gmail.com>
2013-03-01 07:05:22 -0500

    added soundmanager files, not yet hooked in, not sure why

--- demo/source/javascripts/app.js
+++ demo/source/javascripts/app.js
@@ -31,6 +31,20 @@ $(document).ready(function()
 
 	*/
 
+
+	threeSixtyPlayer.config = {
+	  playNext: false, // stop after one sound, or play through list until end
+	  autoPlay: false, // start playing the first sound right away
+	  allowMultiple: true, // let many sounds play at once (false = one at a time)
+	  loadRingColor: '#ccc',// amount of sound which has loaded
+	  playRingColor: '#000', // amount of sound which has played
+	  backgroundRingColor: '#eee', // "default" color shown underneath everything else
+	  animDuration: 500,
+	  animTransition: Animator.tx.bouncy// http://www.berniecode.com/writing/animator.html
+	}
+
+
+
 });
 
 
