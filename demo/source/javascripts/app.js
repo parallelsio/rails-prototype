5af351ca34234d873dfb6b55d0f382c589ccb406
Steven J. Dale <lifeinchords@gmail.com>
2013-03-07 17:02:31 -0500

    added svg partial for testing, got animate and flip working

---
+++ demo/source/javascripts/app.js
@@ -0,0 +1,108 @@
+$(document).ready(function()
+{
+	
+	// d3.select("#sm2-container").insert("svg", ":first-child")
+ //           .attr("width", 500)
+ //           .attr("height", 50);
+
+
+    soundManager.setup({
+      url: '/swf/',
+      flashVersion: 8, // optional: shiny features (default = 8)
+      useFlashBlock: false
+      
+    });
+
+
+
+	threeSixtyPlayer.config = {
+	  playNext: false, // stop after one sound, or play through list until end
+	  autoPlay: false, // start playing the first sound right away
+	  allowMultiple: true, // let many sounds play at once (false = one at a time)
+	  loadRingColor: '#ccc',// amount of sound which has loaded
+	  playRingColor: '#000', // amount of sound which has played
+	  showHMSTime: true,
+	  useEQData: true,
+	  useWaveformData: true,
+	  backgroundRingColor: '#eee', // "default" color shown underneath everything else
+	  animDuration: 500,
+	  animTransition: Animator.tx.bouncy// http://www.berniecode.com/writing/animator.html
+	}
+
+
+
+
+
+	$('#play_opening').click(function(){
+		
+		var soundObject = soundManager.createSound({
+		 id:'opening',
+		 url:'/sounds/opening-v1.mp3'
+		
+		});
+
+		soundObject.play();
+		return false;
+	});
+
+
+
+	$( ".content" ).draggable({ handle: "p" });
+
+	/*
+	
+	$(".content").mouseover(function(){
+
+			
+			Mousetrap.bind('tab', function(e, element){
+			    console.log(e);
+			    
+
+			    // This is not working
+			    // $(this).attr('id').effect("highlight", {}, 3000);
+			    
+			    return false;
+				});
+
+
+    	// $(this).removeClass().addClass("hover");
+    }).mouseout(function(){
+
+    	// $(this).removeClass().addClass("hover");		
+    	Mousetrap.reset();
+    });
+
+	*/
+
+
+	$("._21").click(function(){
+  		$("._21 .content").animate({	
+  				top: 	'-=20px',
+  			 	left: 	'+=183px'  },
+  			 "slow",
+  			 function(){
+               //$(this).html("Animation Completed");
+               $(this).unbind('click');  }
+  		);
+	}); 
+
+
+    $('._23').click(function(){
+        $(this).find('.card').addClass('flipped').mouseleave(function(){
+            $(this).removeClass('flipped');
+        });
+        return false;
+    });
+
+	// $('._23').hover(function(){
+	// 			$(this).addClass('flip');
+	// 		},function(){
+	// 			$(this).removeClass('flip');
+	// 		});
+			
+
+			
+
+
+});
+
