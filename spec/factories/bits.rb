0664e0c3efcba9626ff2ab3c1153316fff3e700f
Steven J. Dale <lifeinchords@macbook-pro-3.local>
2013-08-19 15:44:09 -0400

    fixing position bug in spec

--- spec/factories/bits.rb
+++ spec/factories/bits.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
 		# as this factory generates new bits,
 		# use sequence to cascade them so they
 		# dont overlap and hide each other
-		sequence(:position_x) { |n| n + 50 }
+		sequence(:position_y) { |n| n + 50 }
 		sequence(:position_x) { |n| n + 50 }
 
 	end
